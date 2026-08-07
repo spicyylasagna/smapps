4823a585e3b7b69c2e6f30a690f300f7369e6ec1
spicyylasagna <141018498+spicyylasagna@users.noreply.github.com>
2026-08-07 21:12:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/smapps/',
   optimizeDeps: {
     exclude: ['maplibre-gl']
   }
